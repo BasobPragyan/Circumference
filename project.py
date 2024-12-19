7043608ba6e95d26f2c80e08e47e15edee24201f
BasobPragyan <pragyan95prabodhbanerjee@gmail.com>
2024-12-19 23:14:24 +0530

    Circumference and Area calculator 344

---
+++ project.py
@@ -0,0 +1,12 @@
+def circumference():
+    rad=int(input("Enter radius : "))
+    cir=2*3.14*rad
+    print("Cicumference of circle is : ",cir)
+def areacir():
+    rad=int(input("Enter radius : "))
+    area=3.14*rad**2
+    print("Area is : ",area)
+
+rad1=int(input("Enter radius : "))
+circumference()
+areacir()
